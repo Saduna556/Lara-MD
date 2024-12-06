d2f98ba6890d9fda34f73c6ad34b993fa547bd9e
Saduna556 <sadunlakmal1099@gmail.com>
2024-12-06 09:47:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,12 +5,12 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "b75TSTZC#z19LzQtUycxLarV7tdEHbRW4Tl5C6yV-1ojav9P7bJE", //paste your session_id
+SESSION_ID: process.env.SESSION_ID || "byZkQZDC#AcSCo_zJBJzUiOXf2LOtJjFT2iKi_2UNJqurpJLB2PE", //paste your session_id
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false", 
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
-AUTO_STICKER: process.env.AUTO_STICKER || "false",
-AUTO_REPLY: process.env.AUTO_REPLY || "false",
-AUTO_REACT: process.env.AUTO_REACT || "false",
+AUTO_STICKER: process.env.AUTO_STICKER || "true",
+AUTO_REPLY: process.env.AUTO_REPLY || "true",
+AUTO_REACT: process.env.AUTO_REACT || "true",
 FAKE_RECORDING: process.env.FAKE_RECORDING || "true",    
 };
